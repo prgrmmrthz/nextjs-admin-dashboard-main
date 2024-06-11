4215947cb5731f653522fd8807ae1674ec543e2b
prgrmmrthz <prgrmmrthz>
2024-06-12 00:28:18 +0800

    created database

---
+++ src/app/auth/register/RegisterForm.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import { RegisterSchema, registerSchema } from '@/lib/schemas/registerSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Card, CardBody, CardHeader, Input } from '@nextui-org/react'
+import React from 'react'
+import { useForm } from 'react-hook-form';
+import { GiPadlock } from 'react-icons/gi'
+
+export default function RegisterForm() {
+
+    const { register, handleSubmit, setError, formState: { errors, isValid, isSubmitting } } = useForm<RegisterSchema>({
+        resolver: zodResolver(registerSchema),
+        mode: 'onTouched'
+    });
+
+    const onSubmit = async (data: RegisterSchema) => {
+        console.log(data);
+    }
+    
+    return (
+        <Card className='w-2/5 mx-auto'>
+            <CardHeader className='flex flex-col items-center justify-center'>
+                <div className='flex flex-col gap-2 items-center text-secondary'>
+                    <div className='flex flex-row items-center gap-3'>
+                        <GiPadlock size={30} />
+                        <h1 className='text-3xl font-semibold'>Register</h1>
+                    </div>
+                    <p className='text-neutral-500'>Welcome to IntL POS</p>
+                </div>
+            </CardHeader>
+            <CardBody>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className='space-y-4'>
+                        <Input
+                            defaultValue=''
+                            label='Name'
+                            variant='bordered'
+                            {...register('name')}
+                            isInvalid={!!errors.name}
+                            errorMessage={errors.name?.message}
+                        />
+                        <Input
+                            defaultValue=''
+                            label='Email'
+                            variant='bordered'
+                            {...register('email')}
+                            isInvalid={!!errors.email}
+                            errorMessage={errors.email?.message}
+                        />
+                        <Input
+                            defaultValue=''
+                            label='Password'
+                            variant='bordered'
+                            type='password'
+                            {...register('password')}
+                            isInvalid={!!errors.password}
+                            errorMessage={errors.password?.message}
+                        />
+                        {errors.root?.serverError && (
+                            <p className='text-danger text-sm'>{errors.root.serverError.message}</p>
+                        )}
+                        <Button
+                            isLoading={isSubmitting}
+                            isDisabled={!isValid}
+                            fullWidth color='secondary' type='submit'
+                        >
+                            Register
+                        </Button>
+                    </div>
+                </form>
+            </CardBody>
+        </Card>
+    )
+}
